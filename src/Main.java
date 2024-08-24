f6cc35ca4fa583a4e580001d3d006fbf2a5092e2
LeonelLima <leonel_mk@yahoo.com.br>
2024-08-24 11:25:09 -0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import java.awt.Color;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        //Creating and configuring a window frame
+        JFrame mainWindow = new JFrame(); //Creates a frame
+        mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Makes it so the frame closes when click X
+        mainWindow.setResizable(false); //Prevent frame from resize
+        mainWindow.setSize(420,420); // Sets the x and y dimension of the frame
+        mainWindow.setTitle("Bank App"); // Sets title of the frame
+        mainWindow.setVisible(true); //Makes frame visible
+
+        //Customizing how the window frame looks
+        ImageIcon icon = new ImageIcon("Images/AppIcon.png"); //Create the icon with an image
+        mainWindow.setIconImage(icon.getImage()); //Change icon of the frame
+        mainWindow.getContentPane().setBackground(new Color(0x1E1E1E));
+    }
+}
